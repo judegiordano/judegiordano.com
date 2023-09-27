d0340cc9b13ed72f68fb8143a5c3bdb18cc11ff2
judegiordano <judegiordano@gmail.com>
2023-09-27 14:15:52 -0400

    Link To Sitemap

--- ui/layout/Footer.tsx
+++ ui/layout/Footer.tsx
@@ -6,13 +6,16 @@ export const Footer = () => {
 		<footer className="p-5 text-white bg-black footer place-items-center">
 			<div className="place-items-center">
 				<div className="grid grid-flow-col gap-4">
-					{/* github */}
 					<Link href="https://github.com/judegiordano" target="_blank" aria-label="github">
 						github
 					</Link>
-					{/* linkedin */}
 					<Link href="https://www.linkedin.com/in/jude-giordano-868728107" target="_blank" aria-label="linkedin">
 						linkedin
+					</Link>
+				</div>
+				<div>
+					<Link href="/sitemap.xml" aria-label="sitemap">
+						sitemap
 					</Link>
 				</div>
 				<span className="footer-title">Copyright © {new Date().getFullYear()}</span>
