5ce6d9c2ee8e77d41c24fb98e85692f3b9a749c1
judegiordano <judegiordano@gmail.com>
2023-09-27 13:46:23 -0400

    NavBar

---
+++ ui/layout/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const NavBar = async () => {
+	return (
+		<div className='pb-5 text-center pt-7 bg-none'>
+			<Link className='px-3 font-normal btn btn-ghost' href="/">home</Link>
+			<Link className='px-3 font-normal btn btn-ghost' href="/about">about</Link>
+		</div>
+	)
+}
